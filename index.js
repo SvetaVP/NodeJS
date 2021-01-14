332d13cc2a4a60c9ace34d54912d6b68f046b3db
Sveta <qwe7vbn12@gmail.com>
2021-01-15 00:18:57 +0200

    familiarization with the capabilities of node.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const greeting = require("./greeting");
+const User = require("./user.js");
+
 http
   .createServer(function (request, response) {
     response.end("Hello NodeJS!");
@@ -6,3 +9,7 @@ http
   .listen(3000, "127.0.0.1", function () {
     console.log("Сервер начал прослушивание запросов на порту 3000");
   });
+
+console.log(`Дата запроса: ${greeting.date}`);
+let eugene = new User("Eugene", 32);
+eugene.sayHi();
